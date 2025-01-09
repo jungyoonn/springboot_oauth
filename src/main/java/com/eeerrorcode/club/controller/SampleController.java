9e9f4110a43a47a779f097124267eb3fa7db350f
jungyoonn <sophia7625@naver.com>
2025-01-09 18:26:21 +0900

    security 샘플 컨트롤러

---
+++ src/main/java/com/eeerrorcode/club/controller/SampleController.java
@@ -0,0 +1,43 @@
+package com.eeerrorcode.club.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.eeerrorcode.club.security.dto.AuthMemberDto;
+
+
+@Controller
+@RequestMapping("sample")
+@Log4j2
+public class SampleController {
+  @GetMapping("all")
+  public void exAll() {
+    // 인증, 인가에 필요한 객체들
+    // UsernamePasswordAuthenticationToken token;
+    // AuthenticationManager manager;
+    // AuthenticationProvider provider;
+    log.info("ex all");
+  }
+  
+  @GetMapping("member")
+  public void exMember() {
+    log.info("ex member");
+  }
+  
+  @GetMapping("admin")
+  public void exAdmin() {
+    log.info("ex admin");
+  }
+  
+  @GetMapping("api")
+  @ResponseBody
+  public AuthMemberDto getMethodName(@AuthenticationPrincipal AuthMemberDto dto) {
+    return dto;
+  }
+  
+}
