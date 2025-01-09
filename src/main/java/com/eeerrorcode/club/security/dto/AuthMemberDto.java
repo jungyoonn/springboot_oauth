df964fe2893ca4fbe7f6ce1a3d1435f2c711295d
jungyoonn <sophia7625@naver.com>
2025-01-09 18:29:35 +0900

    auth dto

---
+++ src/main/java/com/eeerrorcode/club/security/dto/AuthMemberDto.java
@@ -0,0 +1,28 @@
+package com.eeerrorcode.club.security.dto;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AuthMemberDto  extends User{
+  private Long mno;
+  private String email;
+  private String name;
+  private boolean fromSocial;
+
+  public AuthMemberDto(String username, String password, Long mno, boolean fromSocial, String name,Collection<? extends GrantedAuthority> authorities) {
+    super(username, password, authorities);
+    this.name = name;
+    this.mno = mno;
+    this.email = username;
+    this.fromSocial = fromSocial;
+  }
+}
