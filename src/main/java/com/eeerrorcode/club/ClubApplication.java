0de00e6d3cc9054c8dbcc869f6875945fea0c7b2
jungyoonn <sophia7625@naver.com>
2025-01-10 12:15:25 +0900

    어노테이션 추가

--- src/main/java/com/eeerrorcode/club/ClubApplication.java
+++ src/main/java/com/eeerrorcode/club/ClubApplication.java
@@ -3,9 +3,11 @@ package com.eeerrorcode.club;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 
 @SpringBootApplication
 @EnableJpaAuditing // regDate, modDate 적용 어노테이션
+@EnableGlobalAuthentication
 public class ClubApplication {
 
 	public static void main(String[] args) {
