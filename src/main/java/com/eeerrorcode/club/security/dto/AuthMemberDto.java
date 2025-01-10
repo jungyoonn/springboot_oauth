d430c20eb4af95c213e457ddd32f0b3392688109
jungyoonn <sophia7625@naver.com>
2025-01-10 11:27:21 +0900

    implement 추가, OAuth2 호출 생성자 추가

--- src/main/java/com/eeerrorcode/club/security/dto/AuthMemberDto.java
+++ src/main/java/com/eeerrorcode/club/security/dto/AuthMemberDto.java
@@ -1,9 +1,11 @@
 package com.eeerrorcode.club.security.dto;
 
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,17 +14,35 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class AuthMemberDto  extends User{
+public class AuthMemberDto  extends User implements OAuth2User{
   private Long mno;
   private String email;
   private String name;
   private Boolean fromSocial;
+  private Map<String, Object> attr;
 
+  // security 자체 로그인 호출 생성자
   public AuthMemberDto(String username, String password, Long mno, Boolean fromSocial, String name,Collection<? extends GrantedAuthority> authorities) {
     super(username, password, authorities);
     this.name = name;
     this.mno = mno;
     this.email = username;
     this.fromSocial = fromSocial;
+  }
+  
+  // OAuth2 호출 생성자
+  public AuthMemberDto(String username, String password, Long mno, Boolean fromSocial, String name,Collection<? extends GrantedAuthority> authorities
+  , Map<String, Object> attr) {
+    super(username, password, authorities);
+    this.name = name;
+    this.mno = mno;
+    this.email = username;
+    this.fromSocial = fromSocial;
+    this.attr = attr;
+  }
+
+  @Override
+  public Map<String, Object> getAttributes() {
+    return attr;
   }
 }
