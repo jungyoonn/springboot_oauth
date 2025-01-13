0c89c39c9521760c07e1fd6a7584b5179dd939dc
jungyoonn <sophia7625@naver.com>
2025-01-13 17:22:40 +0900

    config  수정

--- src/main/java/com/eeerrorcode/club/config/SecurityConfig.java
+++ src/main/java/com/eeerrorcode/club/config/SecurityConfig.java
@@ -3,23 +3,58 @@ package com.eeerrorcode.club.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.eeerrorcode.club.security.filter.ApiCheckFilter;
+import com.eeerrorcode.club.security.filter.ApiLoginFilter;
+import com.eeerrorcode.club.security.handler.ApiLoginFailHandler;
 import com.eeerrorcode.club.security.handler.LoginSuccessHandler;
+import com.eeerrorcode.club.security.util.JWTUtil;
 
 
 @Configuration
 // @EnableWebSecurity
 @EnableMethodSecurity
 // @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig{
   @Autowired
   private UserDetailsService userDetailsService;
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
+    AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    builder.userDetailsService(userDetailsService)
+      .passwordEncoder(encoder()).setBuilder(builder);
+  
+    AuthenticationManager authenticationManager = builder.build();
+    return authenticationManager;
+  }
+  
+  @Bean
+  public ApiCheckFilter apiCheckFilter(){
+    return new ApiCheckFilter("/api/v1/**", jwtUtil());
+  }
+  
+  @Bean
+  public JWTUtil jwtUtil() {
+    return new JWTUtil();
+  }
+
+  @Bean
+  public ApiLoginFilter apiLoginFilter(AuthenticationManager authenticationManager) throws Exception{
+    ApiLoginFilter apiLoginFilter = new ApiLoginFilter("/api/login", jwtUtil());
+    apiLoginFilter.setAuthenticationManager(authenticationManager);
+    apiLoginFilter.setAuthenticationFailureHandler(new ApiLoginFailHandler());
+    return apiLoginFilter;
+  }
 
   @Bean
   public PasswordEncoder encoder() {
@@ -29,22 +64,26 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(csrf ->  csrf.disable()) // CSRF 비활성화 (필요에 따라 활성화) // 토큰 방식 비활성화라는 뜻
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/sample/all").permitAll() // `/public/` 경로는 인증 없이 접근 가능
-            .requestMatchers("/sample/member").hasRole("USER")
-            .requestMatchers("/api/**").permitAll()
-            // .requestMatchers("/sample/admin").hasRole("ADMIN")
-            .anyRequest().authenticated() // 나머지는 인증 필요
-        )
-        .formLogin(f -> f.permitAll()) // 기본 로그인 폼 활성화
-        .logout(l -> l.logoutUrl("/member/signout"))
-        .oauth2Login(o -> o.successHandler(loginSuccessHandler())) 
-        .rememberMe(r -> r.tokenValiditySeconds(60 * 60 * 24 * 14) // 토큰 유지 시간 (밀리초)
-          .userDetailsService(userDetailsService)
-          .rememberMeCookieName("remember-id")
-        );
-        return http.build();
+      .csrf(csrf ->  csrf.disable()) // CSRF 비활성화 (필요에 따라 활성화) // 토큰 방식 비활성화라는 뜻
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/sample/all").permitAll() // `/public/` 경로는 인증 없이 접근 가능
+        .requestMatchers("/sample/member").hasRole("USER")
+        // .requestMatchers("/api/**").permitAll()
+        // .requestMatchers("/sample/admin").hasRole("ADMIN")
+        // .anyRequest().authenticated() // 나머지는 인증 필요
+        .anyRequest().permitAll()
+      )
+      .formLogin(f -> f.permitAll()) // 기본 로그인 폼 활성화
+      .logout(l -> l.logoutUrl("/member/signout"))
+      .oauth2Login(o -> o.successHandler(loginSuccessHandler())) 
+      .rememberMe(r -> r.tokenValiditySeconds(60 * 60 * 24 * 14) // 토큰 유지 시간 (밀리초)
+        .userDetailsService(userDetailsService)
+        .rememberMeCookieName("remember-id")
+      );
+    http
+      .addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class) // Json 객체 형식이 나오지 않고 필터 적용 로그만 찍힘
+      .addFilterBefore(apiLoginFilter(authenticationManager(http)), UsernamePasswordAuthenticationFilter.class);
+      return http.build();
   }
 
   @Bean
