84bc4db1a195901c9d839acbe8efa46b6208808c
jungyoonn <sophia7625@naver.com>
2025-01-14 09:56:17 +0900

    swagger 페이지 권한 적용

--- src/main/java/com/eeerrorcode/club/config/SecurityConfig.java
+++ src/main/java/com/eeerrorcode/club/config/SecurityConfig.java
@@ -68,6 +68,7 @@ public class SecurityConfig{
       .authorizeHttpRequests(auth -> auth
         .requestMatchers("/sample/all").permitAll() // `/public/` 경로는 인증 없이 접근 가능
         .requestMatchers("/sample/member").hasRole("USER")
+        .requestMatchers("swagger-ui.html").permitAll()
         // .requestMatchers("/api/**").permitAll()
         // .requestMatchers("/sample/admin").hasRole("ADMIN")
         // .anyRequest().authenticated() // 나머지는 인증 필요
