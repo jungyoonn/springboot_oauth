c666aaa315688a905435c34fbe854842d7284bbb
jungyoonn <sophia7625@naver.com>
2025-01-15 17:23:13 +0900

    첨부파일 리포지토리

---
+++ src/main/java/com/eeerrorcode/club/repository/AttachRepository.java
@@ -0,0 +1,9 @@
+package com.eeerrorcode.club.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eeerrorcode.club.entity.Attach;
+
+public interface AttachRepository extends JpaRepository<Attach, String>{
+  
+}
