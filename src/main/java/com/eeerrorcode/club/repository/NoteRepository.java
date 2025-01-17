e9c497ea6770ea022936ce4fce2d8d6cec4898ba
jungyoonn <sophia7625@naver.com>
2025-01-17 16:03:59 +0900

    좋아요 갯수, 첨부파일 갯수 가져오는 jpql 추가

--- src/main/java/com/eeerrorcode/club/repository/NoteRepository.java
+++ src/main/java/com/eeerrorcode/club/repository/NoteRepository.java
@@ -3,6 +3,8 @@ package com.eeerrorcode.club.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.eeerrorcode.club.entity.Note;
 
@@ -12,4 +14,21 @@ public interface NoteRepository extends JpaRepository<Note, Long>{
   List<Note> findByMemberMno(Long mno);
   
   List<Note> findByMemberEmail(String email);
+
+  @Query(
+        "select n, count(DISTINCT l) as likescnt, count(DISTINCT a) as attachcnt \r\n" + //
+        "from tbl_note n left join tbl_likes l on l.note.num = n.num left join tbl_attach a on a.note.num = n.num \r\n" + //
+        "where n.member.email = :email group by n")
+  // @Query("select n, COUNT(DISTINCT l.note.num, l.member.mno) as likescnt, COUNT(DISTINCT uuid) as attachcnt from tbl_note n \r\n" + //
+  //       "left join tbl_member m on n.member.mno = m.mno and email = :email\r\n" + //
+  //       "left join tbl_likes l on l.note.num = n.num \r\n" + //
+  //       "left join tbl_attach a on a.note.num = n.num \r\n" + //
+  //       "group by n.num")
+  List<Object[]> findNotesBy(@Param("email") String email);
+
+  @Query(
+        "select n, count(DISTINCT l) as likescnt, count(DISTINCT a) as attachcnt \r\n" + //
+        "from tbl_note n left join tbl_likes l on l.note.num = n.num left join tbl_attach a on a.note.num = n.num \r\n" + //
+        "group by n")
+  List<Object[]> findNotes();
 }
