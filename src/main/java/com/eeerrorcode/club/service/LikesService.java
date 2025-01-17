5c4b2e0a67c1517f91459c4e13963854a43e020b
jungyoonn <sophia7625@naver.com>
2025-01-17 11:18:15 +0900

    Likes service 인터페이스 생성

---
+++ src/main/java/com/eeerrorcode/club/service/LikesService.java
@@ -0,0 +1,29 @@
+package com.eeerrorcode.club.service;
+
+import com.eeerrorcode.club.entity.Likes;
+import com.eeerrorcode.club.entity.Member;
+import com.eeerrorcode.club.entity.Note;
+import com.eeerrorcode.club.entity.dto.LikesDto;
+
+public interface LikesService {
+  void toggle(LikesDto dto);
+
+  boolean get(LikesDto dto);
+
+  default LikesDto toDto(Likes likes) {
+    return LikesDto.builder()
+      .mno(likes.getMember().getMno())
+      .email(likes.getMember().getEmail())
+      .num(likes.getNote().getNum())
+      .regDate(likes.getRegDate())
+      .modDate(likes.getModDate())
+    .build();
+  }
+
+  default Likes toEntity(LikesDto dto) {
+    return Likes.builder()
+      .member(Member.builder().mno(dto.getMno()).build())
+      .note(Note.builder().num(dto.getNum()).build())
+    .build();
+  }
+}
